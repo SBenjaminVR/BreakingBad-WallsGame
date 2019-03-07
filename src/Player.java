9b11846ebfcc0d507961942f56c2c3044adf3ea1
Humberto Gonzalez <hglez.98s@gmail.com>
2019-03-06 23:23:19 -0600

    Added Player comments

--- src/Player.java
+++ src/Player.java
@@ -15,16 +15,25 @@ import java.awt.Rectangle;
  */
 
 public class Player extends Item {
-    private int direction;
-    private int width;
-    private int height;
-    private Game game;
-    private int speed;
-    private Rectangle hitbox;
-    private Animation bar; // to store the animation for going up
-    public enum playerState { init, alive, dead }
-    private playerState state;
+    private int direction; // Direction of the player
+    private int width;      // Width of the player
+    private int height;     // Height of the player
+    private Game game;      // Game object
+    private int speed;      // Speed of the player
+    private Rectangle hitbox;// The player's hitbox
+    private Animation bar; // to store the animation for the bar
+    public enum playerState { init, alive, dead } // Declares the player's possible states
+    private playerState state; // To store the player's current state
     
+    /**
+     * Constructor with parameters
+     * @param x
+     * @param y
+     * @param direction
+     * @param width
+     * @param height
+     * @param game 
+     */
     public Player(int x, int y, int direction, int width, int height, Game game) {
         super(x, y);
         this.direction = direction;
@@ -36,48 +45,80 @@ public class Player extends Item {
         this.bar = new Animation(Assets.playerBar, 100);
         this.state = playerState.init;
     }
-     
+     /**
+      * Returns the width of the player
+      * @return 
+      */
     public int getWidth() {
         return width;
     }
-    
+    /**
+     * Returns the height of the player
+     * @return 
+     */
     public int getHeight() {
         return height;
     }
-    
+    /**
+     * Sets the direction of the player
+     * @param direction 
+     */
     public void setDirection(int direction) {
         this.direction = direction;
     }
-    
+    /**
+     * Sets the width of the player
+     * @param width 
+     */
     public void setWidth(int width) {
         this.width = width;
     }
-    
+    /**
+     * Sets the height of the player
+     * @param height 
+     */
     public void setHeight(int height) {
         this.height = height;
     }
-
+    /**
+     * Returns the speed of the player
+     * @return 
+     */
     public int getSpeed() {
         return speed;
     }
-
+    /**
+     * Sets the speed of the player
+     * @param speed 
+     */
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+    /**
+     * Returns the player's hitbox
+     * @return 
+     */
     public Rectangle getHitbox() {
         return hitbox;
     }
-
+    /**
+     * Returns the player's current state
+     * @return 
+     */
     public playerState getState() {
         return state;
     }
-
+    /**
+     * Sets the player state
+     * @param state 
+     */
     public void setState(playerState state) {
         this.state = state;
     }
-    
-    
-    
+        
+    /**
+     * Function tick that gets called every frame and updates player logic
+     */
     @Override
     public void tick() {
         
@@ -113,8 +154,10 @@ public class Player extends Item {
         }       
     }
     
-    
-    
+    /**
+     * Function render that draws the player
+     * @param g 
+     */    
     @Override
     public void render(Graphics g) {
        g.drawImage(bar.getCurrentFrame(), getX(), getY(), getWidth(), getHeight(), null);
